acff127df99171970722d116a850cdeea1aeb8a6
Will Leahy <procterw@gmail.com>
2023-11-18 15:36:45 -0800

    Adds skeleton for watch app extension

---
+++ pd-watch Watch App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  pd-watch Watch App
+//
+//  Created by William Leahy on 11/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ pd-watch Watch App/pd_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  pd_watchApp.swift
+//  pd-watch Watch App
+//
+//  Created by William Leahy on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct pd_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
